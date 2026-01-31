20d44bfbc3d1e6876a6241b03a936b12a8dc79e9
Aareen80085 <aareend@gmail.com>
2026-01-31 11:19:00 +0530

    chore: Remove .DS_Store and add a .gitignore file to exclude common C++ build artifacts.

--- OOPS.cpp
+++ OOPS.cpp
@@ -433,3 +433,19 @@ using namespace std;
 //   p.print("Hello");
 //   return 0;
 // }
+
+// OPERATOR OVERLOADING(USING OPERATORS)
+
+class Number {
+public:
+  int x;
+
+  Number(int a) { x = a; }
+  Number operator+(Number obj) { return Number(x + obj.x); }
+};
+int main() {
+  Number n1(10), n2(20);
+  Number n3 = n1 + n2;
+
+  cout << n3.x << endl;
+}
---
+++ array.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+using namespace std;
+
+// Two dimensional Array
+// int main() {
+//   int a[2][3];
+//   cout << "Enter elements of 2D array:\n";
+
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 3; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+
+//   cout << "Array elements are:\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (intj = 0)
+//   }
+// }
+
+// PROGRAM 2
+//  int main() {
+//    int a[2][2], sum = 0;
+//    cout << "Enter elements of 2D array:\n";
+
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       cin >> a[i][j];
+//       sum += a[i][j];
+//     }
+//   }
+//   cout << "Sum of elements= " << sum;
+//   return 0;
+// }
+
+// PROGRAM 3
+
+// int main() {
+//   int a[2][2], b[2][2], c[2][2];
+//   cout << "Enter first matrix:\n";
+
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+//   cout << "Enter the second matrix: \n";
+//   for (int i; i < 2; i++) {
+//     for (int j; j < 2; j++) {
+//       cin >> b[i][j];
+//     }
+//   }
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       c[i][j] = a[i][j] + b[i][j];
+//     }
+//   }
+//   cout << "Result matrix:\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       cout << c[i][j] << " ";
+//     }
+//     cout << endl;
+//   }
+
+//   return 0;
+// }
+
+// MUTIPICATION OF 2D ARRAYS
+
+// int main() {
+//   int a[2][3], b[3][2], c[2][2];
+//   cout << "Enter the first martix:\n";
+
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 3; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+//   cout << "Enter the second matrix: \n";
+//   for (int i; i < 3; i++) {
+//     for (int j; j < 2; j++) {
+//       cin >> b[i][j];
+//     }
+//   }
+//   cout << "Result matrix:\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       c[i][j] = 0;
+//       for (int k = 0; k < 3; k++) {
+//         c[i][j] += a[i][j] * b[k][j];
+//       }
+//       cout << c[i][j] << " ";
+//     }
+//     cout << endl;
+//   }
+//   return 0;
+// }
+
+// TRANSPOSE OF MATRIX
+// int main() {
+//   int a[2][2];
+//   cout << "Enter first matrix:\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+//   cout << "Transpose of matrix:\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       cout << a[j][i] << " ";
+//     }
+//     cout << endl;
+//   }
+// }
+
+// LARGEST ELEMENT IN THE MATRIX
+// int main() {
+//   int a[2][3];
+//   int max;
+//   cout << "Enter first matrix:\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 3; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+//   max = a[0][0];
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 3; j++) {
+//       if (a[i][j] > max) {
+//         max = a[i][j];
+//       }
+//     }
+//   }
+//   cout << "largest element is: " << max << endl;
+//   return 0;
+// }
+
+// SMALLEST ELEMENT IN THE MATRIX
+// Min
+// int main() {
+//   int a[5];
+//   cout << "Enter array elements:\n";
+//   for (int i = 0; i < 5; i++) {
+//     cin >> a[i];
+//   }
+//   int min = a[0];
+//   for (int i = 1; i < 5; i++) {
+//     if (a[i] < min) {
+//       min = a[i];
+//     }
+//   }
+//   cout << "Smallest element is " << min << endl;
+// }
+
+// SUM OF DIAGONAL ELEMENTS OF THE MATRIX
+// int main() {
+//   int n;
+//   int sum = 0;
+//   cout << "Enter order of matrix: ";
+//   cin >> n;
+//   int a[n][n];
+//   cout << "Enter matrix elements:\n";
+//   for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < n; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+//   for (int i = 0; i < n; i++) {
+//     sum += a[i][i];
+//   }
+//   return 0;
+// }
+
+// int main() {
+//   int arr[2][2] = {{10, 20}, {30, 40}};
+//   int(*p)[2] = arr;
+
+//   cout << "Enter the matrix\n";
+//   for (int i = 0; i < 2; i++) {
+//     for (int j = 0; j < 2; j++) {
+//       cin >> *(*(p + i) + j);
+//     }
+
+//     cout << *(*(p + 0) + 0) << endl; // 10
+//     cout << *(*(p + 1) + 1) << endl; // 40
+//   }
+//   return 0;
+// }
+void search(int rollNo, string name);
+void search() {
+  for (i =, ) {
+  }
+}
+
+int main() {
+  int rollNo[] = {1, 2, 3, 4, 5};
+  string name[] = {"Aareen", "Rahul", "Gaurav", "Ronak", "Veeraj"};
+
+  void
+}
